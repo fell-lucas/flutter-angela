c7c1499e4b61c19e689a26c16e18b98aede05cb6
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-22 17:57:12 -0300

    test verified commit

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Center(
-      child: Text('Hello World'),
+      child: Text('Hello Teste'),
     ),
   ));
 }

19dbf356b18e52ab08362edae44cd3a654e23d21
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-23 15:23:40 -0300

    mi card - phone and email

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -41,6 +41,54 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 10.0,
+                  ),
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        '+55 51 9 8011 1438',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal[900],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 10.0,
+                  ),
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.mail,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        'lucas.fell@zallpy.com',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal[900],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
           ),

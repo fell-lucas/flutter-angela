885f450d24406aae247e0bbc967863ee396c7fe4
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-23 10:22:22 -0300

    I am Rich class 2

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello Teste'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+            'https://www.imagesource.com/wp-content/uploads/2019/06/Rio.jpg',
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blueGrey[300],
     ),
   ));
 }

bb2df2a084d996e8fc5509803c774d0ccc36d67f
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-27 16:59:46 -0300

    xylophone - fix project stub

---
+++ xylophone-flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\lucas.fell\scoop\apps\flutter\2.5.1"
+export "FLUTTER_APPLICATION_PATH=C:\Users\lucas.fell\repos\flutter-angela-course\xylophone-flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
--- xylophone_flutter/android/app/src/main/kotlin/com/example/xylophone_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.xylophone_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

f5a4be0283fc840d3da22de39a3c0305a40322e0
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-27 11:59:12 -0300

    magic 8 ball - app bar and container

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}

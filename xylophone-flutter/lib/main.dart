9b58bfb1dd76b337c91fbe301a25a5b8dcb8ffba
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-28 11:07:56 -0300

    xylophone - finishing with functions

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -4,18 +4,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  Widget buildKey({int note, Color color}) {
+    return Expanded(
+      child: MaterialButton(
+        color: color,
+        onPressed: () {
+          player.play('note$note.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Container(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildKey(color: Colors.red, note: 1),
+                buildKey(color: Colors.orange, note: 2),
+                buildKey(color: Colors.yellow, note: 3),
+                buildKey(color: Colors.green, note: 4),
+                buildKey(color: Colors.teal, note: 5),
+                buildKey(color: Colors.blue, note: 6),
+                buildKey(color: Colors.purple, note: 7),
+              ],
             ),
           ),
         ),

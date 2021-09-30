74e4f84a2d8d1ecbb46d80987344cdf2e169cac6
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-30 13:49:17 -0300

    bmi calc - reusing widgets

--- bmi-calculator-flutter/lib/screens/input_page.dart
+++ bmi-calculator-flutter/lib/screens/input_page.dart
@@ -12,11 +12,54 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  ReusableCard(),
+                  SizedBox(width: 15),
+                  ReusableCard(),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            ReusableCard(),
+            SizedBox(height: 15),
+            Expanded(
+              child: Row(
+                children: [
+                  ReusableCard(),
+                  SizedBox(width: 15),
+                  ReusableCard(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+    this.color = const Color(0xFF1d1f33),
+  }) : super(key: key);
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }

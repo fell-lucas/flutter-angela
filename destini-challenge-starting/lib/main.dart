48a0ab44d6d58f0182e84cba1e187feafedaef1c
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-09-30 11:04:45 -0300

    destini - steps 11 through 22

--- destini-challenge-starting/lib/main.dart
+++ destini-challenge-starting/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -53,12 +51,11 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    sb.nextStory(1);
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    sb.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -75,12 +72,11 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    sb.nextStory(2);
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    sb.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),

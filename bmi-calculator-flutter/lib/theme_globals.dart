874d4619ffe8d86cb3b5d0375f7ed1976dfd3722
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-10-01 14:26:13 -0300

    bmi calc - slider shape and color

--- bmi-calculator-flutter/lib/theme_globals.dart
+++ bmi-calculator-flutter/lib/theme_globals.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF090c22);
 const kBottomNavHeight = 80.0;
 const kRedAccentColor = Color(0xFFEB1555);
-const kRedAccentTransparentColor = Color(0x66EB1555);
+const kRedAccentTransparentColor = Color(0x29EB1555);
 const kActiveBg = Color(0xFF1D1E33);
 const kInactiveBg = Color(0xFF111328);
 const kInactiveText = Color(0xFF8F9098);

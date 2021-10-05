c5c31ba16a881fbfaccd3e65a9ffcb26431789e1
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-10-05 15:20:02 -0300

    clima - spinkit

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,17 +20,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
           'https://api.openweathermap.org/data/2.5/weather?lat=${loc.latitude}&lon=${loc.longitude}&appid=$apiKey',
     );
 
-    print(await nh.getData());
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(),
+      ),
+    );
   }
 
   @override
   void initState() {
     super.initState();
+    getLocation();
   }
 
   @override
   Widget build(BuildContext context) {
-    getLocation();
-    return Scaffold();
+    return Scaffold(
+      body: SpinKitDoubleBounce(
+        color: Colors.white,
+        size: 100.0,
+      ),
+    );
   }
 }

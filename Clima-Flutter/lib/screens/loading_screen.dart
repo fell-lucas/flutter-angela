d5434bd0ac4c59fda13dfa07b58d8b74457c3930
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-10-05 14:45:47 -0300

    clima - json challenge

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,17 +13,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location loc = Location();
     await loc.getCurrentLocation();
-    print([loc.latitude, loc.longitude]);
+
+    Response response = await get(
+      'https://api.openweathermap.org/data/2.5/weather?lat=${loc.latitude}&lon=${loc.longitude}&appid=277ea5e7e84f8b7406a57256fddb61d0',
+    );
+    if (response.statusCode == 200) {
+      var decoded = jsonDecode(response.body);
+
+      var temp = decoded["main"]["temp"];
+      var condition = decoded["weather"][0]["id"];
+      var name = decoded["name"];
+      print([temp, condition, name]);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
   }
 
   @override
   Widget build(BuildContext context) {
+    getLocation();
     return Scaffold();
   }
 }

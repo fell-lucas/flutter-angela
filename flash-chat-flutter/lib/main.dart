7866bbb026f22b9c9bf7d446b8f90fba20cd42fc
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-10-07 16:34:08 -0300

    flash chat - firebase config and user register

--- flash-chat-flutter/lib/main.dart
+++ flash-chat-flutter/lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
@@ -13,6 +18,7 @@ class FlashChat extends StatelessWidget {
       theme: ThemeData.light().copyWith(
         textTheme: TextTheme(
           bodyText2: TextStyle(color: Colors.black54),
+          subtitle1: TextStyle(color: Colors.black54),
         ),
       ),
       initialRoute: WelcomeScreen.route,
--- flash-chat-flutter/lib/screens/registration_screen.dart
+++ flash-chat-flutter/lib/screens/registration_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,11 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +38,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kInputDecoration.copyWith(
                 hintText: 'Enter your email',
@@ -42,8 +51,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kInputDecoration.copyWith(
                 hintText: 'Enter your password',
@@ -54,8 +65,15 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             RoundedButton(
               color: Colors.blueAccent,
-              onTap: () {
-                //Implement register functionality.
+              onTap: () async {
+                UserCredential newUser =
+                    await _auth.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                if (newUser != null) {
+                  Navigator.pushNamed(context, ChatScreen.route);
+                }
               },
               text: 'Register',
             ),
